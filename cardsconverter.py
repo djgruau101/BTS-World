84aba608523d721a993b16332f25f87254584070
Daniel Luo <55007645+djgruau101@users.noreply.github.com>
2024-10-14 22:39:37 -0400

    Update cardsconverter.py
    Handled case where no file of name cards_managername.txt is present, simplified the process of not proceeding when roll call is activated, allowed for the program to effectively parse cards when the user copy-pastes all contents of the BTS World Calculator webpage

--- cardsconverter.py
+++ cardsconverter.py
@@ -1,3 +1,5 @@
+import os
+
 members = ["RM", "Jin", "Suga", "J-Hope", "Jimin", "V", "Jungkook"]
 initials = {"RM":"RM", "Jin":"SJ", "Suga":"SG", "J-Hope":"JH", "Jimin":"JM", "V":"V", "Jungkook":"JK"}
 
@@ -29,65 +31,84 @@ wisdomexception = [('Jin', '5', 'FAKE LOVE Jin'), ('Jimin', '5', 'FAKE LOVE Jimi
                    ('V', '1', 'Nice to Meet You')]
 
 exception = [empathyexception, passionexception, staminaexception, wisdomexception]
-#print(sum([len(e) for e in exception]))
-##########################################################################################
-manager = input("Manager name: ")
-fp = open("cards_{}.txt".format(manager), 'r', encoding='utf-8')
-groupstats = list()
-c = list()
-names = list()
-curline = fp.readline()
 
-while len(groupstats) < 4: # get group stats
-    if curline.strip().isdigit():
-        groupstats.append(int(curline.strip()))
+manager_name = input("This program takes your cards' data from the BTS World Calculator and will store them in this program.\nManager name: ")
+if os.path.isfile("cards_{}.txt".format(manager_name)):
+    fp = open("cards_{}.txt".format(manager_name), 'r', encoding='utf-8')
+        
+    groupstats = list()
+    c = list()
+    names = list()
     curline = fp.readline()
 
-while not (len(curline.strip().split('\t')) == 2 and curline.strip().split('\t')[0] in members and curline.strip().split('\t')[1][0].isdigit()):
-    curline = fp.readline()
+    while not curline.strip().startswith("Roll Call"):
+        curline = fp.readline()
+    
+    curline = fp.readline()  # skip over the Roll Call line
+    rollcallinfo = []
 
-n = 0
-clock = False
-while curline != '' and not clock: # process cards
-    member, star = curline.strip().split("\t")
-    star = int(star[0])
-    name = fp.readline().strip()
-    if chr(9200) in name:
+    while len(rollcallinfo) < 3:
+        if curline.strip():
+            rollcallinfo.append(curline.strip())
+        curline = fp.readline()
+
+    clock = False
+
+    if rollcallinfo[-1] != '0':
         clock = True
         input("Make sure you turn off roll call before copy-pasting! Press Enter to exit. ")
-    if name in ["Deep in Thought", "Pit-A-Pat", "Let's Go"]:
-        name = initials[member]+name
-    elif name == "All Eyes on Me":
-        name += str(star)
-    ### JK has 2 cards called All Eyes on Me
-    ### JK and Jin both have a Pit-A-Pat, Deep in Thought; RM and V both have Let's Go
-    level = int(fp.readline().strip())
-    stats = [int(n) for n in fp.readline().strip().replace(',','').split('\t')[:-1]]
-    strength = stats.index(max(stats))
-    for i in range(len(exception)):
-        if (member, str(star), name) in exception[i]:
-            strength = i
-            break
-    c.append([member, name, star, stats[0], stats[1], stats[2], stats[3], traits[strength], level])
-    curline = fp.readline()
 
-fp.close()
-### CONVERT POINTS TO LEVEL:
-if not clock:
-    fw = open("BTS_World_{}.txt".format(manager), 'w', encoding='utf-8')
-    levels = list()
-    for p in groupstats:
-        if (p-103)%13 == 0: # level 50+
-            levels.append((p-103)//13+1)
-        elif (p-100)%13 == 0:
-            levels.append((p-100)//13+1)
-    fw.write(str(levels)[1:-1]+"\n")
+    while len(groupstats) < 4: # get group stats
+        if curline.strip().isdigit():
+            groupstats.append(int(curline.strip()))
+        curline = fp.readline()
 
-    for l in c:
-        for i in range(len(l)):
-            fw.write(str(l[i]))
-            if i != 8:
-                fw.write("; ")
-        fw.write('\n')
-    input("Successfully created BTS_World_{}.txt, press enter to exit. ".format(manager))
-    fw.close()
+    while not (len(curline.strip().split('\t')) == 2 and curline.strip().split('\t')[0] in members and curline.strip().split('\t')[1][0].isdigit()):
+        curline = fp.readline()
+
+    n = 0
+    while (curline != '' and not curline.startswith("Card database")) and not clock: # process cards
+        member, star = curline.strip().split("\t")
+        star = int(star[0])
+        name = fp.readline().strip()
+        if name in ["Deep in Thought", "Pit-A-Pat", "Let's Go"]:
+            name = initials[member]+name
+        elif name == "All Eyes on Me":
+            name += str(star)
+        # JK has 2 cards called All Eyes on Me
+        # JK and Jin both have a Pit-A-Pat, Deep in Thought; RM and V both have Let's Go
+        level = int(fp.readline().strip())
+        stats = [int(n) for n in fp.readline().strip().replace(',','').split('\t')[:-1]]
+        strength = stats.index(max(stats))
+        for i in range(len(exception)):
+            if (member, str(star), name) in exception[i]:
+                strength = i
+                break
+        c.append([member, name, star, stats[0], stats[1], stats[2], stats[3], traits[strength], level])
+        curline = fp.readline()
+
+    fp.close()
+    ### CONVERT POINTS TO LEVEL:
+    if not clock:
+        fw = open("BTS_World_{}.txt".format(manager_name), 'w', encoding='utf-8')
+        levels = list()
+        for p in groupstats:
+            if (p-103)%13 == 0: # level 50+
+                levels.append((p-103)//13+1)
+            elif (p-100)%13 == 0:
+                levels.append((p-100)//13+1)
+        fw.write(str(levels)[1:-1]+"\n")
+
+        for l in c:
+            for i in range(len(l)):
+                fw.write(str(l[i]))
+                if i != 8:
+                    fw.write("; ")
+            fw.write('\n')
+        input("Successfully created BTS_World_{}.txt, press enter to exit. ".format(manager_name))
+        fw.close()
+
+else:
+    fp = open("cards_{}.txt".format(manager_name), 'w+', encoding='utf-8')
+    input("Please copy-paste cards data from the BTS World Calculator to cards.{}.txt. Press enter to exit. ".format(manager_name))
+    fp.close()
